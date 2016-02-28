c2476dea10d245016fb5e380153dd0d196f59c9e
xyw <xuyuanwei@gmail.com>
2016-02-29 07:31:51 +0800

    init.el rewrite from scratch; rename init.el from tuhdo to init.el.tuhdo

---
+++ emacsSelfSettings.el
@@ -0,0 +1,101 @@
+;; setting of emacs it self
+
+;; Available C style:
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(make-backup-files nil))
+ ;; “gnu”: The default style for GNU projects
+;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
+;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
+;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
+;; “stroustrup”: What Stroustrup, the author of C++ used in his book
+;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
+;; “linux”: What the Linux developers use for kernel development
+;; “python”: What Python developers use for extension modules
+;; “java”: The default style for java-mode (see below)
+;; “user”: When you want to define your own style
+(setq
+ c-default-style "linux" ;; set style to "linux"
+ )
+
+
+;; replace set mark hot key to avoid the conflict with input method
+(global-unset-key (kbd "C-SPC"))
+(global-set-key (kbd "M-SPC") 'set-mark-command)
+
+;;reference:
+;;1. http://homepages.inf.ed.ac.uk/s0243221/emacs/
+
+(global-hl-line-mode 1);; In every buffer, the line which contains the cursor will be fully highlighted
+
+
+
+;; ===== Set standard indent to 2 rather that 4 ====
+(setq standard-indent 2)
+
+;; ========== Line by line scrolling ==========
+
+;; This makes the buffer scroll by only a single line when the up or
+;; down cursor keys push the cursor (tool-bar-mode) outside the
+;; buffer. The standard emacs behaviour is to reposition the cursor in
+;; the center of the screen, but this can make the scrolling confusing
+
+(setq scroll-step 1)
+
+;;
+;; Emacs normally uses both tabs and spaces to indent lines. If you
+;; prefer, all indentation can be made from spaces only. To request this,
+;; set `indent-tabs-mode' to `nil'. This is a per-buffer variable;
+;; altering the variable affects only the current buffer, but it can be
+;; disabled for all buffers.
+
+;;
+;; Use (setq ...) to set value locally to a buffer
+;; Use (setq-default ...) to set value globally
+;;
+(setq-default indent-tabs-mode nil)
+
+;; ========== Support Wheel Mouse Scrolling ==========
+(mouse-wheel-mode t)
+
+;; ========== Prevent Emacs from making backup files ==========
+(setq make-backup-files nil)
+
+;; ========== Place Backup Files in Specific Directory ==========
+
+;; Enable versioning with default values (keep five last versions, I think!)
+(setq version-control t)
+
+;; Save all backup file in this directory.
+(setq backup-directory-alist (quote ((".*" . "~/.emacs_backups/"))))
+
+;; ========== Enable Line and Column Numbering ==========
+
+;; Show line-number in the mode line
+(line-number-mode 1)
+
+;; Show column-number in the mode line
+(column-number-mode 1)
+
+
+;; Enable backup files.
+(setq-default fill-column 72)
+
+;; Auto-fill-mode the the automatic wrapping of lines and insertion of
+;; newlines when the cursor goes over the column limit.
+
+;; This should actually turn on auto-fill-mode by default in all major
+;; modes. The other way to do this is to turn on the fill for specific modes
+;; via hooks.
+
+(setq auto-fill-mode nil)
+
+(global-linum-mode)
